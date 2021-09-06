55db2f4423248731e3309d7298a549adc5b28790
Samuel Lisboa <samuellisboa97@hotmail.com>
2021-09-05 22:16:13 -0300

    c4

--- CyberShockServer/src/cybershockserver/CyberShockServer.java
+++ CyberShockServer/src/cybershockserver/CyberShockServer.java
@@ -43,25 +43,55 @@ public class CyberShockServer {
                         System.out.println(entradaDados);
                         String DADOS[] = entradaDados.split("<shock>");
                         
+                        // bruno
                         if(DADOS[1].equals("0")){
                             if(DADOS[0].equals("get")){
-                                out.println(um);
+                                try{
+                                    if(zero==0){
+                                        int reciveInt = Integer.parseInt(DADOS[2]);
+                                        if(reciveInt==zero){
+                                            out.println("#ok");
+                                            zero = 0;
+                                        }else{
+                                            out.println("#errou");
+                                        }
+                                    }else{
+                                        out.println("#offline");
+                                    }
+                                }catch(Exception e){
+                                    out.println("#exception");
+                                }
                             }else if(DADOS[0].equals("set")){
                                 try{
-                                    zero = Integer.parseInt(DADOS[1]);
-                                    out.println("#valorSetOK");
+                                    zero = Integer.parseInt(DADOS[2]);
+                                    out.println("#valorSetOK["+zero+"]");
                                 }catch(Exception e){
                                     out.println("#invalidValor");
                                 }
                                
                             }
+                            //samuel
                         }else if(DADOS[1].equals("1")){
                             if(DADOS[0].equals("get")){
-                                out.println(zero);
+                                try{
+                                    if(um==0){
+                                        int reciveInt = Integer.parseInt(DADOS[2]);
+                                        if(reciveInt==um){
+                                            out.println("#ok");
+                                            um = 0;
+                                        }else{
+                                            out.println("#errou");
+                                        }
+                                    }else{
+                                        out.println("#offline");
+                                    }
+                                }catch(Exception e){
+                                    out.println("#exception");
+                                }
                             }else if(DADOS[0].equals("set")){
                                  try{
-                                    um = Integer.parseInt(DADOS[1]);
-                                    out.println("#valorSetOK");
+                                    um = Integer.parseInt(DADOS[2]);
+                                    out.println("#valorSetOK["+um+"]");
                                 }catch(Exception e){
                                     out.println("#invalidValor");
                                 }
@@ -70,7 +100,7 @@ public class CyberShockServer {
                             out.println("#comandoinvalid");
                         }
                         
-                        
+                        System.out.println("0["+zero+"] 1["+um+"]");
                                                 
                         
                     }catch(Exception e){
